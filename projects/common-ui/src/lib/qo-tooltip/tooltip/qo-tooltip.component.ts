f0a0f8b6e223a4c9a6a434b1514768703e6ed95e
Roberto Sánchez <rsanchez@qualityobjects.com>
2020-08-27 18:30:14 +0200

    Refactoring appTooltip

--- projects/common-ui/src/lib/qo-tooltip/tooltip/qo-tooltip.component.ts
+++ projects/common-ui/src/lib/qo-tooltip/tooltip/qo-tooltip.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit, Input, Directive, HostListener, ComponentRef, ElementRef, OnDestroy } from '@angular/core';
-import { ActivatedRoute, ActivatedRouteSnapshot, UrlSegment } from '@angular/router';
+import { Component, OnInit, Input, Directive, HostListener, ComponentRef, ElementRef, OnDestroy, ViewContainerRef } from '@angular/core';
 import { OverlayRef, Overlay, OverlayPositionBuilder, ConnectedPosition } from '@angular/cdk/overlay';
 import { ComponentPortal } from '@angular/cdk/portal';
 
@@ -7,72 +6,87 @@ import { ComponentPortal } from '@angular/cdk/portal';
 @Directive({ selector: '[appTooltip]' })
 export class AppTooltipDirective implements OnInit, OnDestroy {
 
-  private overlayRef: OverlayRef;
-
+  private overlayRef: OverlayRef | null;
+  private _portal: ComponentPortal<AppTooltipComponent> | null;
+  private _tooltipInstance: AppTooltipComponent | null;
   /**
    * Texto a mostrar en el tooltip
    */
   @Input('appTooltip') text = '';
-  @Input('ttDisabled') ttDisable = true;
 
   /**
    * Determina la orientación del tooltip respecto del elemento referenciado, por defecto 'top', 
    * El tooltip aparece encima del  elemento
    */
-  @Input('ttOrientation') orientation : 'bottom' | 'top' | 'left' | 'right' = 'top';
+  @Input('ttOrientation') orientation: 'bottom' | 'top' | 'left' | 'right' = 'top';
 
   @HostListener('mouseenter')
-  show() { 
-  // Create tooltip portal
-    if (!!this.text && !this.ttDisable) {
-      const tooltipPortal = new ComponentPortal(QOTooltipComponent);
+  show() {
+    // Create tooltip portal
+    if (!!this.text) {
+      this.createIfNecessaryOverlayRef();
+      this._portal = this._portal || new ComponentPortal(AppTooltipComponent, this.viewContainerRef);
 
       // Attach tooltip portal to overlay
-      const tooltipRef: ComponentRef<QOTooltipComponent> = this.overlayRef.attach(tooltipPortal);
-        
+      this.overlayRef.detach();
+      const tooltipRef: ComponentRef<AppTooltipComponent> = this.overlayRef.attach(this._portal);
+      this._tooltipInstance = tooltipRef.instance;
       // Pass content to tooltip component instance
-      tooltipRef.instance.text = this.text;   
-      tooltipRef.instance.orientation = this.orientation;    
+      this._tooltipInstance.text = this.text;
+      this._tooltipInstance.orientation = this.orientation;
+      this._tooltipInstance.visible = true;
     }
   }
 
   @HostListener('mouseleave')
-  hide() { 
-    this.overlayRef.detach();
+  hide() {
+    this.overlayRef?.detach();
+    if (!!this._tooltipInstance) {
+      this._tooltipInstance.visible = false;
+    }
   }
-  
+
   constructor(
     private overlayPositionBuilder: OverlayPositionBuilder,
-    private elementRef: ElementRef,
-    private overlay: Overlay) {    
+    private viewContainerRef: ViewContainerRef,
+    private elementRef: ElementRef<HTMLElement>,
+    private overlay: Overlay) {
   }
 
-  ngOnDestroy() : void {
+  ngOnDestroy(): void {
     this.hide();
+    this.overlayRef?.dispose();
+    this.overlayRef = null;
   }
 
-  ngOnInit() : void {
+  ngOnInit(): void {
+
+  }
+
+  private createIfNecessaryOverlayRef(): OverlayRef {
+    if (!!this.overlayRef) {
+      return this.overlayRef;
+    }
     let position: ConnectedPosition = {
       originX: 'center',
-      originY: 'bottom',
+      originY: 'top',
       overlayX: 'center',
-      overlayY: 'top',
+      overlayY: 'bottom',
     }
-    
     if (this.orientation !== 'top') {
       if (this.orientation === 'bottom') {
-        position.originY = 'top';
-        position.overlayY = 'bottom';
+        position.originY = 'bottom';
+        position.overlayY = 'top';
       } else {
         position.originY = 'center';
         position.overlayY = 'center';
-          if (this.orientation === 'right') {
+        if (this.orientation === 'right') {
           position.originX = 'end';
           position.overlayX = 'start';
         } else if (this.orientation === 'left') {
           position.originX = 'start';
           position.overlayX = 'end';
-        } 
+        }
       }
     }
     const positionStrategy = this.overlayPositionBuilder
@@ -82,20 +96,22 @@ export class AppTooltipDirective implements OnInit, OnDestroy {
       // Means, attach overlay's center bottom point to the         
       // top center point of the elementRef.
       .withPositions([position]);
-      this.overlayRef = this.overlay.create({ positionStrategy });
-  }
+    this.overlayRef = this.overlay.create({ positionStrategy });
 
+    return this.overlayRef;
+  }
 }
 
 @Component({
   selector: 'app-tooltip',
-  templateUrl: './qo-tooltip.component.html',
-  styleUrls: ['./qo-tooltip.component.scss']
+  templateUrl: './app-tooltip.component.html',
+  styleUrls: ['./app-tooltip.component.scss']
 })
-export class QOTooltipComponent {
-  @Input() text : string = '';
+export class AppTooltipComponent {
+  @Input() text: string = '';
   public orientation = 'top';
-  public showShadow : boolean = false;
-  public showArrow : boolean = true;
-  @Input() ttDisable = true;
+  public showShadow: boolean = false;
+  public showArrow: boolean = true;
+
+  public visible: boolean = false;
 }
