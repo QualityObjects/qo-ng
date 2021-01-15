f1f29fba69056dd84154386566416a1625cdc649
David Gayo <dgayo@qualityobjects.com>
2021-01-15 13:51:21 +0100

    Correccion de validador para funcionamiento bimodal de DNIs y NIEs

--- projects/common-ui/src/lib/validators/validator-dni-nie.ts
+++ projects/common-ui/src/lib/validators/validator-dni-nie.ts
@@ -8,20 +8,19 @@ export function dniNieValidator(control: AbstractControl) {
     if(value !== null && value !== ''){
         value = value.padStart(9, '0');
         value = value.toUpperCase().replace(/\s/, '');
-        let niePrefix:string = value.charAt(0);
-        let niePrefix1:number = 0;
+        let niePrefix:string|number = value.charAt(0);
         switch (niePrefix) {
             case 'X':
-                niePrefix1 = 0;
+                niePrefix = 0;
                 break;
             case 'Y':
-                niePrefix1 = 1;
+                niePrefix = 1;
                 break;
             case 'Z':
-                niePrefix1 = 2;
+                niePrefix = 2;
                 break;
         }
-        value = niePrefix1 + value.substr(1);
+        value = niePrefix + value.substr(1);
         if (validRegex.test(value)) {
             valid = (value.charAt(8) === dniLetters.charAt(parseInt(value, 10) % 23));
         }
