eaebebd67b7dc88a99b23d4c7d6b3b0305046fd3
Roberto Sánchez <rsanchez@qualityobjects.com>
2021-09-30 20:48:28 +0200

    FIX the-table min-width

--- projects/the-table/src/lib/the-table/the-table.component.ts
+++ projects/the-table/src/lib/the-table/the-table.component.ts
@@ -469,7 +469,8 @@ export class TheTableComponent implements OnInit {
    * @param flex
    */
   public getColumnWidth(flex: number): number {
-    return flex / this.totalVisibleColumnsWidth() * (this.hasCheckboxColumn() ? 85 : 90);
+    console.log('flex:', flex, ', totalVisibleColumnsWidth: ', this.totalVisibleColumnsWidth(), ', minus: ', (100 - (this.actionsColFlex + (this.hasCheckboxColumn() ? 5 : 0))))
+    return flex / this.totalVisibleColumnsWidth() * (100 - (this.actionsColFlex + (this.hasCheckboxColumn() ? 5 : 0)));
   }
 
   public hasCheckboxColumn(): boolean {
